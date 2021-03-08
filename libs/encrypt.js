4bf43b3f818b07875472cd68c0750e4bc8fa2526
ruved <ruvedalam7776@gmail.com>
2021-03-09 02:30:33 +0530

    Configuration to deploy to heroku

--- libs/encrypt.js
+++ libs/encrypt.js
@@ -1,9 +1,6 @@
 const crypto = require("crypto");
 
 module.exports.encryptPassword = function(password) {
-  var hash = crypto
-    .createHmac("sha256", password)
-    .update("9743-980")
-    .digest("hex");
+  var hash = crypto.createHmac("sha256", password).update("9743-980").digest("hex");
   return hash;
 };
